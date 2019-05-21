642d27bf83abf29f736aac584835667b9442db80
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-05-21 13:45:03 -0700

    Initialized Unity project and Shadergraph

---
+++ Assets/NoiseGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator : MonoBehaviour
+{
+    public Material matToChange;
+    public Texture noise;
+
+    private void Update()
+    {
+        matToChange.SetTexture("DisplacementMap", noise);
+    }
+}
