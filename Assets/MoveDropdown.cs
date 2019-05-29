d2ad5d2ec1713042f693c1643c411972cca7bcee
Max Fischer <42ducksable1@gmail.com>
2019-05-29 11:00:48 -0700

    Changed animation on ui

---
+++ Assets/MoveDropdown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDropdown : MonoBehaviour
+{
+    private Animator animator;
+    public Vector3 closePosition;
+    public Vector3 openPosition;
+    public RuntimeAnimatorController m_animation;
+    private bool state = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void StopOpen()
+    {
+        //animator.runtimeAnimatorController = null;
+        GetComponent<RectTransform>().anchoredPosition = openPosition;
+        animator.SetBool("Open", false);
+    }
+
+    public void StopClose()
+    {
+        //animator.runtimeAnimatorController = null;
+        GetComponent<RectTransform>().anchoredPosition = closePosition;
+        animator.SetBool("False", false);
+    }
+
+}
