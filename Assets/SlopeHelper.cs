3ea664b01f853dd5f8a2b4e7820effab6911914e
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2019-06-03 12:12:58 -0700

    Changed tile hitboxes and added player support for slopes

---
+++ Assets/SlopeHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlopeHelper : MonoBehaviour
+{
+    public Vector2 addForceMultiplier = new Vector2(0, 1f);
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetInputs inputs = new GetInputs();
+            if(inputs.hInput != 0)
+            {
+                other.GetComponent<Rigidbody2D>().velocity = other.GetComponent<Rigidbody2D>().velocity + addForceMultiplier;
+                Debug.Log("Push");
+            }
+        }
+    }
+
+}
